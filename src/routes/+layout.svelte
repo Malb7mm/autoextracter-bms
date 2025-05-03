6b15fa88f37ff3a918e93bf1f604ae241e9586b9
Malb7mm <ykmeak9g@gmail.com>
2025-05-03 16:14:26 +0900

    ヘッダー作成

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,46 @@
 <header>
-  AutoExtracter-BMS
+  <button class="logs">
+    ログ
+  </button>
+  <button class="settings">
+    設定
+  </button>
 </header>
 <main>
   <slot/>
 </main>
 
 <style>
-  header {
-    background-color: #eee;
-  }
+
+:global(body) {
+  background-color: hsl(0, 0%, 97%);
+}
+
+header {
+  user-select: none;
+  height: 30px;
+  padding: 0 15px;
+
+  display: flex;
+  align-items: center;
+  column-gap: 10px;
+
+  background-color: hsl(0, 0%, 97%);
+  border-bottom: solid 1px #aaa;
+  box-shadow: 0 0 3px #aaa;
+  font-size: 0.8rem;
+  color: #222;
+}
+
+header > button {
+  background-color: #aaa0;
+  height: 100%;
+  padding: 0 10px;
+  transition: background-color 0.1s;
+}
+
+header > button:hover {
+  background-color: #aaa3;
+}
+
 </style>
